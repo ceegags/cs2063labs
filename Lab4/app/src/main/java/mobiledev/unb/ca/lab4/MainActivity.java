f9bf9005a6962fd826e29b829c7e1d7f1d1af2a7
coreyganong <coreyganong@gmail.com>
2016-02-01 23:29:48 -0400

    Lab 4

--- Lab4/app/src/main/java/mobiledev/unb/ca/lab4/MainActivity.java
+++ Lab4/app/src/main/java/mobiledev/unb/ca/lab4/MainActivity.java
@@ -1,13 +1,50 @@
 package mobiledev.unb.ca.lab4;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button aButton;
+    private Button bButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aButton = (Button) findViewById(R.id.fragmentAButton);
+        aButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                Fragment fragment = new FragmentA();
+                fragmentTransaction.add(R.id.fragment_container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        bButton = (Button) findViewById(R.id.fragmentBButton);
+        bButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                Fragment fragment = new FragmentB();
+                fragmentTransaction.add(R.id.fragment_container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
     }
 }
